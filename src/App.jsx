ed0d18644cfa6810a3730078176690835f6bfbb9
AlaaEddineRAHALI <ralaaeddine1@gmail.com>
2023-04-24 14:57:46 +0200

    commitSectionNav

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import SectionNav from "./components/SectionNav/SectionNav";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-  
-      <div>
-       hello world
+    <div>
+      <SectionNav />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
