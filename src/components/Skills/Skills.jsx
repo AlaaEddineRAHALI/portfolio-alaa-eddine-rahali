7819e7de1a7ad5940139e04c19f7819ec44f03ec
AlaaEddineRAHALI <ralaaeddine1@gmail.com>
2023-05-09 22:18:18 +0200

    projects_optimisation

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -5,16 +5,16 @@ const Skills = () => {
   return (
     <section
       id="skills"
-      className=" bg-black px-5  py-[15%] sm:px-2 md:py-[6%]  md:pb-[42%] lg:py-[05%] lg:pb-[8%]"
+      className=" bg-black px-5  py-[15%] sm:px-2 md:py-[6%]  md:pb-[55%] lg:py-[04%] lg:pb-[09%]"
     >
       <div className="  text-white ">
         <div className="mx-auto  max-w-screen-xl px-4  sm:px-6 sm:py-12 md:px-11 lg:px-8 ">
           <div className=" text-left  ">
-            <h2 className="mt-6 pb-4 text-3xl font-bold sm:text-6xl">
+            <h2 className="mt-6  text-3xl font-bold sm:pb-5 sm:text-6xl">
               Compétences
             </h2>
 
-            <p className="mt-8  text-xl text-gray-300">
+            <p className="mt-8  text-base text-gray-300 sm:text-2xl ">
               Passionné par les nouvelles technologies, je suis constamment en
               quête d&apos;amélioration de mes compétences techniques et de mon
               savoir-faire
@@ -22,12 +22,12 @@ const Skills = () => {
           </div>
 
           <div className="mx-5 mt-8 grid grid-cols-1 gap-10 sm:mx-1 md:grid-cols-3 lg:grid-cols-3  ">
-            <div className="   block h-full rounded-xl bg-[#D9D9D9] p-8 shadow-xl transition hover:border-gray-200/10  hover:shadow-gray-200/10 md:w-full lg:w-full">
+            <div className="   block h-full rounded-xl bg-[#D9D9D9] mx-6  md:mx-0 p-8 shadow-xl transition hover:border-gray-200/10  hover:shadow-gray-200/10 w-5/6 md:w-full lg:w-full">
               <h2 className="mt-4 text-center text-2xl font-bold text-[#0172B1] underline underline-offset-8 sm:text-2xl ">
                 Développement Backend
               </h2>
 
-              <ol className="skills mt-4 text-xl text-[#554A4A]">
+              <ol className="skills mt-4 text-base text-[#554A4A]">
                 <li className="skills py-1">NodeJS</li>
                 <li className="skills py-1">Express</li>
                 <li className="skills py-1">Insomnia</li>
@@ -35,24 +35,24 @@ const Skills = () => {
                 <li className="skills py-1">Sequelize</li>
               </ol>
             </div>
-            <div className="   block h-full rounded-xl bg-[#D9D9D9] p-8 shadow-xl transition hover:border-gray-200/10  hover:shadow-gray-200/10 md:w-full lg:w-full">
+            <div className="   block h-full rounded-xl bg-[#D9D9D9] mx-6  md:mx-0 p-8 shadow-xl transition hover:border-gray-200/10  hover:shadow-gray-200/10 w-5/6 md:w-full lg:w-full">
               <h2 className="mt-4 text-center text-2xl font-bold text-[#0172B1] underline underline-offset-8 ">
                 Développement FrontEnd
               </h2>
 
-              <ol className="skills mt-4 text-xl text-[#554A4A]">
+              <ol className="skills mt-4 text-base text-[#554A4A]">
                 <li className="skills py-1">HTML5</li>
                 <li className="skills py-1">CSS3</li>
                 <li className="skills py-1">Javascript</li>
                 <li className="skills py-1">React</li>
               </ol>
             </div>
-            <div className="   block h-full rounded-xl bg-[#D9D9D9] p-8 shadow-xl transition hover:border-gray-200/10  hover:shadow-gray-200/10 md:w-full lg:w-full">
+            <div className="   block h-full rounded-xl bg-[#D9D9D9] mx-6  md:mx-0 p-8 shadow-xl transition hover:border-gray-200/10  hover:shadow-gray-200/10 w-5/6 md:w-full lg:w-full">
               <h2 className="mt-4 text-center text-2xl font-bold text-[#0172B1] underline underline-offset-8">
                 Gestion de projet
               </h2>
 
-              <ol className="skills mt-4 text-xl text-[#554A4A]">
+              <ol className="skills mt-4 text-base text-[#554A4A]">
                 <li className="skills py-1">Méthode Agile Scrum</li>
                 <li className="skills py-1">JIRA</li>
                 <li className="skills py-1">GANTT</li>
