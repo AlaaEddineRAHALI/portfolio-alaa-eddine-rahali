b8f8f9fbbe97a28be8eac862224cc5fa47fc43a9
AlaaEddineRAHALI <ralaaeddine1@gmail.com>
2023-05-25 11:36:58 +0200

    fix repsonsive phone

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,7 +5,7 @@ import "./aboutMeStyles.css";
 const AboutMe = () => {
   return (
     <section id="about" className="  px-5 lg:pt-[9%] ">
-      <div className="pb-[45%] pt-20  md:pb-[35%] md:pt-[30%] lg:pb-[20%] lg:pt-[0%]">
+      <div className="pb-[45%] pt-20  md:pb-[35%] md:pt-[30%] lg:pb-[23%] lg:pt-[0%]">
         <div className="flex flex-col items-center ">
           <div className="card h-56 w-44 md:h-80 md:w-64 xl:h-48 xl:w-36 2xl:h-80 2xl:w-64">
             <div className="imgBx">
