a82ae35001ccf72e0ec4327381ec26136b8e5113
AlaaEddineRAHALI <ralaaeddine1@gmail.com>
2023-05-25 12:01:40 +0200

    fix repsonsive phone

--- src/components/Experiences/Experiences.jsx
+++ src/components/Experiences/Experiences.jsx
@@ -17,7 +17,7 @@ import "react-vertical-timeline-component/style.min.css";
 function Experiences() {
   return (
     <>
-      <section id="experiences" className=" px-5 lg:pt-[9%]  ">
+      <section id="experiences" className=" px-5 lg:pt-[7%]  ">
         <div className="px-6 pt-24 md:pt-32 lg:pt-[0%]">
           <p className=" p-2 text-left text-3xl font-bold sm:pb-4 lg:text-center xl:text-4xl 2xl:text-6xl">
             Ingénieur Consultant{" "}
